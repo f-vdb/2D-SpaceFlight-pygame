6a07795087cf558e0d644817389008544d77da1e
fvdb <frank@am-knie.de>
2020-02-08 19:38:18 +0100

    refactored: + gamefunctions besser strukturiert

--- gameFunctions.py
+++ gameFunctions.py
@@ -1,24 +1,31 @@
 import sys
 import pygame
 
+
+def checkKeyDownEvents(event, myShip):
+    if event.key == pygame.K_RIGHT:
+        myShip.movingRight = True
+    elif event.key == pygame.K_LEFT:
+        myShip.movingLeft = True
+    elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+        sys.exit()
+
+def checkKeyUpEvents(event, myship):
+    if event.key == pygame.K_RIGHT:
+        myship.movingRight = False
+    elif event.key == pygame.K_LEFT:
+        myship.movingLeft = False
+
 def checkEvents(myShip):
     """Respond to keypress and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
-                sys.exit()
-            elif event.key == pygame.K_RIGHT:
-                myShip.movingRight = True
-            elif event.key == pygame.K_LEFT:
-                myShip.movingLeft = True
-
+            checkKeyDownEvents(event, myShip)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                myShip.movingRight = False
-            elif event.key == pygame.K_LEFT:
-                myShip.movingLeft = False
+            checkKeyUpEvents(event, myShip)
+            
 
 def updateScreen(mySettings, screen, ship):
     """Update images on the screen and flip to the new screen."""
